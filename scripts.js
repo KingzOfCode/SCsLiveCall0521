ed5b68092ff73f7840b2820a7744b20545ac4507
Shayon <shayon@comcast.net>
2020-05-21 22:36:54 -0400

    update

--- scripts.js
+++ scripts.js
@@ -2,8 +2,12 @@
 // let clock = document.getElementsByClassName("clock")
 // clock[0].innerHTML = date;
 
-setInterval(function(){
-    let date = new Date().toLocaleDateString();
-    let clock = document.getElementsByClassName('clock').innerHTML= date;
+// setInterval(function(){
+  //  let date = new Date().toLocaleDateString();
+  //  let clock = document.getElementsByClassName('clock').innerHTML= date;
     // console.log(new Date().toLocaleDateString())
-}, 1000)
+// }, 1000)
+
+setInterval(function(){
+    document.getElementById("clock").innerHTML = clock.innerHTML = new Date().toLocaleTimeString();
+       }, 1000)
